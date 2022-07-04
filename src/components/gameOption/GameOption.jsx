a395706b31879d2435ec3fbbc8ae87916b460396
Edmar Junio <juninhocarrijo@live.com>
2022-07-04 15:03:31 -0300

    Criado status gameOptions

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,17 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption () {
+const GameIcon = ({iconName}) => <Icon iconName ={iconName} size="22px" />
+
+function GameOption ({status}) {
     return (
         <div className={styles.gameOption}>
-            x
+            {
+                status === 1 && <GameIcon iconName="circle" />
+            }
+            {
+                status === -1 && <GameIcon iconName="x" />
+            }
         </div>
     )
 }
